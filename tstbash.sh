28d0887f485c0a7cf30cfad8e9b25d9782c74825
Ninad Khargonkar <ninad.khargonkar@gmail.com>
2018-04-25 22:00:29 -0400

    Edited dotfiles

--- tstbash.sh
+++ tstbash.sh
@@ -4,8 +4,8 @@ cp ~/.bashrc ./
 cp ~/.vimrc ./
 cp ~/.emacs.d/init.el ./
 cp ~/.emacs.d/pandoc.css ./
-cp ~/Desktop/emacs-notes.md
-cp ~/Desktop/vim-notes.md
+cp ~/Desktop/emacs-notes.md ./
+cp ~/Desktop/vim-notes.md ./
 
 git add .
 git commit -m "Edited dotfiles"
