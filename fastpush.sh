d6c18da0be7af2bd5ede1ef165cea55f8d63f9da
Ninad Khargonkar <ninad.khargonkar@gmail.com>
2019-11-09 11:49:02 -0600

    updated dotfiles

--- fastpush.sh
+++ fastpush.sh
@@ -3,6 +3,7 @@ cp ~/.tmux.conf ./
 cp ~/.vimrc ./
 cp -r ~/.pandoc/templates/ pandoc-files/
 cp ~/.pandoc/pandoc.css pandoc-files/
+cp ~/.config/Code/User/settings.json vscode_settings.json
 # cp ~/.emacs.d/init.el ./
 # cp -r ~/.emacs.d/org-css/ ./
 
