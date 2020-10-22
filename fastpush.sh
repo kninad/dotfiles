f9229354082a760a7be5cd951a3c1b6776b33006
Ninad Khargonkar <ninad.khargonkar@gmail.com>
2020-10-21 23:29:58 -0500

    updated dotfiles

--- fastpush.sh
+++ fastpush.sh
@@ -2,7 +2,7 @@ cp ~/.bashrc ./bashrc
 # cp ~/.tmux.conf ./tmux.conf
 cp ~/.vimrc ./vimrc
 cp ~/.screenrc ./screenrc
-cp -r /home/ninad/.pandoc/ ./pandoc/
+cp -r /home/ninad/.pandoc/* ./pandoc/
 cp ~/.config/Code/User/settings.json vscode_settings.json
 # cp ~/.emacs.d/init.el ./
 # cp -r ~/.emacs.d/org-css/ ./
